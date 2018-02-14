a3c56799e94779f049afff5cd21bed076eeab521
Betzer09 <31580350+Betzer09@users.noreply.github.com>
2018-02-14 13:45:03 -0700

    Fixed the popover issue on the ipad

--- EveryTeenSeen/View Controller/CreateEventViewController.swift
+++ EveryTeenSeen/View Controller/CreateEventViewController.swift
@@ -8,24 +8,29 @@
 
 import UIKit
 
-class CreateEventViewController: UIViewController, UITextFieldDelegate, UITextViewDelegate {
-    
-    
-    
+protocol PhotoSelectedViewControllerDelegate {
+    func photoSelectedWithVC(_ image: UIImage)
+}
+
+class CreateEventViewController: UIViewController, UITextFieldDelegate, UITextViewDelegate, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+
     // MARK: - Outlets
     @IBOutlet weak var eventTitleTextField: UITextField!
     @IBOutlet weak var locationTextField: UITextField!
     @IBOutlet weak var dateTextField: UITextField!
     @IBOutlet weak var selectedImageView: UIImageView!
+    @IBOutlet weak var eventInfoTextView: UITextView!
+    @IBOutlet weak var pickImageButton: UIButton!
     
     
     // MARK: - Properties
+    var delegate: PhotoSelectedViewControllerDelegate?
+    
     
     // TextField Properties
     let eventDatePicker = UIDatePicker()
     var currentYShiftForKeyboard: CGFloat = 0
     var textFieldBeingEdited: UITextField?
-    
     var textViewBeingEdited: UITextView?
     
 
@@ -44,7 +49,7 @@ class CreateEventViewController: UIViewController, UITextFieldDelegate, UITextVi
     
     // MARK: - Actions
     @IBAction func saveBtnPressed(_ sender: Any) {
-        guard let title = eventTitleTextField.text, let eventDateString = dateTextField.text, let address = locationTextField.text, let user = UserController.shared.loadUserFromDefaults() else {return}
+        guard let title = eventTitleTextField.text, let eventDateString = dateTextField.text, let address = locationTextField.text, let user = UserController.shared.loadUserFromDefaults(), let eventInfo = eventInfoTextView.text else {return}
     
         guard let eventDate = returnFormattedDateFor(string: eventDateString) else {
             presentSimpleAlert(viewController: self, title: "Badly Formatted Date", message: "Be sure not to edit the textfield after you press done.")
@@ -52,14 +57,79 @@ class CreateEventViewController: UIViewController, UITextFieldDelegate, UITextVi
             return
         }
         
-        EventController.shared.saveEventToFireStoreWith(title: title, dateHeld: eventDate, userWhoPosted: user, address: address, eventInfo: "")
+        EventController.shared.saveEventToFireStoreWith(title: title, dateHeld: eventDate, userWhoPosted: user, address: address, eventInfo: eventInfo)
         
         
     }
     
     @IBAction func pickImageBtnPressed(_ sender: Any) {
+        self.pickImageButton.titleLabel?.text = ""
+        self.pickImageButton.tintColor = UIColor.clear
+        
+        self.presentCameraAndPhotoLibraryOption()
     }
     
+    
+    // MARK: - Photo Methods
+    
+    // Picking an iamge from libary
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        
+        guard let image = info[UIImagePickerControllerOriginalImage] as? UIImage else {return}
+        
+        // Assign the iamge in the delegate
+        delegate?.photoSelectedWithVC(image)
+        
+        selectedImageView.image = image
+        dismiss(animated: true, completion: nil)
+        
+    }
+    
+    private func presentCameraAndPhotoLibraryOption() {
+        
+        let actionSheet = UIAlertController(title: "Where do you want your photo from?", message: "", preferredStyle: .actionSheet)
+        
+        let cameraAction = UIAlertAction(title: "Camera", style: .default) { (_) in
+            // Get access to the camara
+            if UIImagePickerController.isSourceTypeAvailable(.camera) {
+                
+                let imagePicker = UIImagePickerController()
+                imagePicker.delegate = self
+                imagePicker.sourceType = .camera
+                imagePicker.allowsEditing = false
+                self.present(imagePicker, animated: true, completion: nil)
+                
+            }
+        }
+        
+        let libarayAction = UIAlertAction(title: "From library", style: .default) { (_) in
+            if UIImagePickerController.isSourceTypeAvailable(.photoLibrary) {
+                let imagePicker = UIImagePickerController()
+                imagePicker.delegate = self
+                imagePicker.sourceType = .photoLibrary
+                imagePicker.allowsEditing = true
+                imagePicker.modalPresentationStyle = .popover
+                
+                if let popoverPresentation = imagePicker.popoverPresentationController {
+                    popoverPresentation.sourceView = self.view
+                    popoverPresentation.sourceRect = CGRect(x: self.view.bounds.midX, y: self.view.bounds.midY / 2, width: 0, height: 0)
+                    popoverPresentation.permittedArrowDirections = .any
+                }
+                
+                self.present(imagePicker, animated: true, completion: nil)
+            }
+        }
+        
+        if let popoverContoller = actionSheet.popoverPresentationController {
+            popoverContoller.sourceView = self.view
+            popoverContoller.sourceRect = CGRect(x: self.view.bounds.midX, y: self.view.bounds.midY / 2, width: 0, height: 0)
+            popoverContoller.permittedArrowDirections = []
+        }
+        
+        actionSheet.addAction(cameraAction)
+        actionSheet.addAction(libarayAction)
+        self.present(actionSheet, animated: true, completion: nil)
+    }
     
     // MARK: - Set Up View
     private func setUpView() {

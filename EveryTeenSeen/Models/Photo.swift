4828fa07c6b70ac446883bb31e2d9932ccfc8aa0
Betzer09 <31580350+Betzer09@users.noreply.github.com>
2018-02-14 17:21:29 -0700

    You can save and load new events

--- EveryTeenSeen/Models/Photo.swift
+++ EveryTeenSeen/Models/Photo.swift
@@ -2,21 +2,8 @@
 //  Photo.swift
 //  EveryTeenSeen
 //
-//  Created by Austin Betzer on 2/13/18.
+//  Created by Austin Betzer on 2/14/18.
 //  Copyright © 2018 Austin Betzer. All rights reserved.
 //
 
 import Foundation
-
-class Photo {
-    
-    // MARK: - Properties
-    let title: String
-    var data: Data?
-    
-    init(title: String, data: Data) {
-        self.title = title
-        self.data = data
-    }
-    
-}
